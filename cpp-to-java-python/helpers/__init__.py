cb21ed08d7719e31ee2516c6e88f0edd4767dfdc
P0Cik <sasha.sergeev.1992@inbox.ru>
2026-01-20 00:18:17 +1000

    Delete cpp-to-java-python directory

--- cpp-to-java-python/helpers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Helper functions for the C++ to Java converter
-"""
-from .type_converter import TypeConverter
-from .name_converter import NameConverter
-from .expression_helpers import ExpressionHelpers
-
-__all__ = ['TypeConverter', 'NameConverter', 'ExpressionHelpers']
